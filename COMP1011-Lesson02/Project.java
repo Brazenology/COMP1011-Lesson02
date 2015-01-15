f1bd8b175c08f4cbb271463efed5c079b2dd2f95
brazenology <brazen101@hotmail.ca>
2015-01-15 16:03:25 -0500

    Instantiated new hero.

---
+++ COMP1011-Lesson02/Project.java
@@ -0,0 +1,14 @@
+
+public class Project {
+
+	public static void main(String[] args) {
+		
+		//Instantiate Hero
+		Hero hero = new hero("Big Man");
+		hero.fight();
+		hero.run();
+		hero.showAbilities();
+
+	}
+
+}
